dfc2dd8acff31414121ea2bc977922196ab8b506
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-10-28 14:27:32 +0300

    додав код 2 роботи

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,60 @@ namespace DZ_WF
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.StartGame = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartGame
+            // 
+            this.StartGame.Location = new System.Drawing.Point(239, 182);
+            this.StartGame.Name = "StartGame";
+            this.StartGame.Size = new System.Drawing.Size(223, 159);
+            this.StartGame.TabIndex = 0;
+            this.StartGame.Text = "Start Game";
+            this.StartGame.UseVisualStyleBackColor = true;
+            this.StartGame.Click += new System.EventHandler(this.StartGame_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(1, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введіть ваше число";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.StartGame);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button StartGame;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,7 +14,32 @@ namespace DZ_WF
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();          
+        }
+
+        private void StartGame_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length==0)
+            {
+                MessageBox.Show($"Введіть своє число");
+            }
+            else
+            {
+                DialogResult result;
+                int numDialog = 1;
+                while (true)
+                {
+                    result = MessageBox.Show($"{new Random().Next(1,20)}", "Ви загадали число", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        MessageBox.Show($"Кількість запитів {numDialog}", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        numDialog = 0;
+                        result = MessageBox.Show($"Хочете продовжити?", "Вихід", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (result == DialogResult.No) this.Close();
+                    }
+                    numDialog++;
+                }
+            }
         }
     }
 }
